70e8c5e0ec54a12854664baa460e6e71dafb81a4
JamesDaniel <jamesmcgarr@protonmail.com>
2020-12-22 20:41:57 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+https://www.npmjs.com/package/node-bluetooth
+
+# history
+
+sudo apt-get install libbluetooth-dev
---
+++ index.js
@@ -0,0 +1,13 @@
+const bluetooth = require('node-bluetooth');
+ 
+// create bluetooth device instance
+const device = new bluetooth.DeviceINQ();
+
+device.listPairedDevices(console.log);
+
+device
+.on('finished',  console.log.bind(console, 'finished'))
+.on('found', function found(address, name){
+  console.log('Found: ' + address + ' with name ' + name);
+}).scan();
+
